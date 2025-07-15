061620165f0bb8a5b259c33b99fab82cda047dfd
Iam Melo <iams.melo06@gmail.com>
2025-07-15 10:58:48 -0300

    feat: adiciona a package de exceções

---
+++ src/Exception/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class InvalidAgeException extends RuntimeException {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exception/InvalidWeightException.java
@@ -0,0 +1,6 @@
+package Exception;
+
+public class InvalidWeightException extends Exception{
+    public InvalidWeightException(String message) {super(message);}
+
+}
